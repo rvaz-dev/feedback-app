ab4ce47a1e3e8a820008ec42a231cae9dd4edff0
rvaz-dev <rvazn.ds@gmail.com>
2022-04-12 15:22:42 -0300

    finished wrapping update

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,8 +1,8 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react'
 
-import RatingSelect from './RatingSelect';
-import Card from './shared/Card';
-import Button from './shared/Button';
+import RatingSelect from './RatingSelect'
+import Card from './shared/Card'
+import Button from './shared/Button'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackForm() {
@@ -11,13 +11,22 @@ function FeedbackForm() {
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
-    const { addFeedback } = useContext(FeedbackContext)
+    const { addFeedback, feedbackEdit, updateFeedback } =
+        useContext(FeedbackContext)
+
+    useEffect(() => {
+        if (feedbackEdit.edit == true) {
+            setBtnDisabled(false)
+            setText(feedbackEdit.item.text)
+            setRating(feedbackEdit.item.rating)
+        }
+    }, [feedbackEdit])
 
     const handleTextChange = (e) => {
         if (text === '') {
             setBtnDisabled(true)
             setMessage(null)
-        } else if(text !== '' && text.trim().length <= 10) {
+        } else if (text !== '' && text.trim().length <= 10) {
             setBtnDisabled(true)
             setMessage('Text must be at least 10 characters')
         } else {
@@ -34,16 +43,18 @@ function FeedbackForm() {
                 text,
                 rating,
             }
-            addFeedback(newFeedback)
+            if (feedbackEdit.edit == true) {
+                updateFeedback(feedbackEdit.item.id, newFeedback)
+            } else {
+                addFeedback(newFeedback)
+            }
             setText('')
         }
     }
 
-
-
     return (
         <Card>
-            <form onSubmit={handleSubmit} >
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
                 <RatingSelect select={(rating) => setRating(rating)} />
                 <div className='input-group'>
@@ -53,16 +64,14 @@ function FeedbackForm() {
                         placeholder='Write a Review'
                         value={text}
                     />
-                    <Button
-                        type='submit'
-                        isDisabled={btnDisabled}>
+                    <Button type='submit' isDisabled={btnDisabled}>
                         Send
                     </Button>
                 </div>
                 {message && <div className='message'>{message}</div>}
             </form>
         </Card>
-    );
+    )
 }
 
-export default FeedbackForm;
+export default FeedbackForm
