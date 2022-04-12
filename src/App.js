03e21c10397286db9bd533446c79511bc557acca
rvaz-dev <rvazn.ds@gmail.com>
2022-04-12 12:45:42 -0300

    gotta feed them'all

--- src/App.js
+++ src/App.js
@@ -1,63 +1,39 @@
-// const title = 'Blog Post'
-// const body = 'This is my blog post'
-// const comments = [
-//     {id: 1, text: 'Coment One' },
-//     {id: 2, text: 'Coment Two' },
-//     {id: 3, text: 'Coment Three'},
-//     {id: 4, text: 'Coment Four'},
-//     {id: 5, text: 'Coment Five'},
-//     {id: 6, text: 'Coment Six'},
-// ]
-
-// const loading = false
-// const showComments = false
-
-// return (
-//     <div>
-//         <h1>{title.toUpperCase()}</h1>
-//         <p>{body}</p>
-//         {/* && qyer dizer que nao tem outras condicoes so se true */}
-//         {showComments && 
-//             <div className='coments'>
-//                 <h3>Comments({comments.length})</h3>
-//                 {comments.map((comment, index) => (
-//                     <li key={index}>{comment.text}</li>
-//                 ))}
-//             </div>
-//         }
-
-
-//     </div>
-// )
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
-import Header from './components/Header'
 
+import Header from './components/Header'
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+	
+    const [feedback, setFeedback] = useState(FeedbackData)
 
-  const deleteFeedback = (id) => {
-	  if (window.confirm('Are you sure you want to delete?')) {
-		setFeedback(feedback.filter((item) => item.id !== id))
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
 	}
-  }
+	
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4()
+		setFeedback([newFeedback, ...feedback])
+	}
 
-
-  return (
-    <>
-      <Header />
-      <div className='container'>
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback}
-          handleDelete={deleteFeedback} />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                />
+            </div>
+        </>
+    )
 }
 export default App
-
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+
+import RatingSelect from './RatingSelect';
 import Card from './shared/Card';
 import Button from './shared/Button';
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
@@ -18,15 +21,28 @@ function FeedbackForm() {
             setMessage(null)
             setBtnDisabled(false)
         }
-
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating,
+            }
+            handleAdd(newFeedback)
+            setText('')
+        }
+    }
+
+
+
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit} >
                 <h2>How would you rate your service with us?</h2>
-                {/* @todo - rating select component */}
+                <RatingSelect select={(rating) => setRating(rating)} />
                 <div className='input-group'>
                     <input
                         onChange={handleTextChange}
