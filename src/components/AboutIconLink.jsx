23aa0771c342b6cd932b25eabc28005d72f18bd8
rvaz-dev <rvazn.ds@gmail.com>
2022-04-12 14:28:21 -0300

    commit

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,22 @@
+import { FaQuestion } from 'react-icons/fa'
+import { Link, NavLink } from 'react-router-dom'
+
+function AboutIconLink() {
+    return (
+		<div className='about-link'>
+			{/* <Link
+                to={{
+					pathname: '/about',
+					search: '?sort=name',
+					hash: '#hello',
+                }}
+            ></Link> */}
+            <Link to='/about'>
+                <FaQuestion size={30} />
+            </Link>
+        </div>
+    )
+}
+	// create an active link in react to show what page is active
+	//<NavLink to='/about' activeClassName='active' />
+export default AboutIconLink
